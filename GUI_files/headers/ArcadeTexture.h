ecbac4288fa2f00ea9037ea5cd7bfe160532d4b4
Geffen Cooper <geffen_cooper@umail.ucsb.edu>
2019-10-25 01:58:32 -0700

    small changes

--- GUI_files/headers/ArcadeTexture.h
+++ GUI_files/headers/ArcadeTexture.h
@@ -1,225 +1,78 @@
-// ArcadeTexture.h
+// GameScreen.h
 
-// This is a utility of the virtual arcade system to help render and handle images and text
-// In general, the ArcadeTexture object is a way to interact with and initialize textures
-// The main purpose of the ArcadeTexture class is to make rendering easier and more organized
+#ifndef GAME_SCREEN
+#define GAME_SCREEN
 
-
-#ifndef ARCADETEXTURE_H
-#define ARCADETEXTURE_H
-
-#include "SDL.h"
-#include "SDL_image.h"
-#include "SDL_ttf.h"
-#include "Config.h"
-#include <string>
-
-class ArcadeTexture
+#include "Screen.h"
+#include "Entity.h"
+class GameScreen : public Screen
 {
 public:
-	ArcadeTexture();
 
-	//Deallocates memory
-	~ArcadeTexture();
-	
-	// ACCESSORS
-	SDL_Rect getImageDestination()
+	virtual ~GameScreen()
 	{
-		return imageDestination;
+		for (int i = 0; i < getArcadeTextureList()->size(); i++)
+		{
+			if ((*getArcadeTextureList())[i] != nullptr)
+			{
+				delete (*getArcadeTextureList())[i];
+				(*getArcadeTextureList())[i] = nullptr;
+			}
+
+		}
+		for (int i = 0; i < entities.size(); i++)
+		{
+			delete entities[i];
+		}
 	}
 
-	TTF_Font* getFontRenderer()
-	{ 
-		return fontRen; 
-	}
+	virtual void newGame() = 0;
 
-	SDL_Texture* getSDLTexture()
+	virtual Action update(SDL_Event* event);
+
+	// method to render the current Screen
+	virtual void render(SDL_Renderer* renderer);
+
+	std::vector<Entity*>* getEntities() { return &entities; }
+	void addEntity(Entity* entity_in) { entities.push_back(entity_in); }
+
+	std::vector<Entity*>* getMovingEntities() { return &movingEntities; }
+	void addMovingEntity(Entity* movingEntity_in) { movingEntities.push_back(movingEntity_in); }
+
+	void setParentNode(void* parentNode_in) { parentNode = parentNode_in; }
+	void* getParentNode() { return parentNode; }
+
+	void setPauseScreen(void* pauseScreen_in) { pauseScreen = pauseScreen_in; }
+	void* getPauseScreen() { return pauseScreen; }
+
+	void setGameOverScreen(void* gameOverScreen_in) { gameOverScreen = gameOverScreen_in; }
+	void* getGameOverScreen() { return gameOverScreen; }
+
+	void setGameState(bool gameState_in) { gameState = gameState_in; }
+	bool getgameState() { return gameState; }
+
+	void setisNewGame(bool isNewGame_in) { isNewGame = isNewGame_in; }
+	bool getIsNewGame() { return isNewGame; }
+
+	void restartGame();
+
+	struct playerScore
 	{
-		return texture;
-	}
-
-	// MUTATORS
-	void setRenderer(SDL_Renderer* renderer_in)
-	{
-		if (renderer_in == nullptr)
-		{
-			printf("WARNING! Setting ArcadeTexture texture renderer to a nullptr.");
-		}
-		texRen = renderer_in;
-	}
-
-	int getX() { return imageDestination.x; }
-	int getY() { return imageDestination.y; }
-	int getW() { return imageDestination.w; }
-	int getH() { return imageDestination.h; }
-
-	void setXPosition(int x_in)
-	{
-		imageDestination.x = x_in;
-	}
-
-	void setYPosition(int y_in)
-	{
-		imageDestination.y = y_in;
-	}
-
-	void setPosition(int x_in, int y_in)
-	{
-		if (x_in == CENTER_X)
-		{
-			x_in = windowWidth / 2 - fileWidth / 2;
-		}
-		if (x_in == CENTER_Y)
-		{
-			x_in = windowHeight / 2 - fileHeight / 2;
-		}
-		imageDestination.x = x_in;
-		imageDestination.y = y_in;
-	}
-
-	void setSize(int w_in, int h_in)
-	{
-		if (w_in == WIDTH_ORIGINAL)
-		{
-			w_in = fileWidth;
-		}
-		if (h_in == HEIGHT_ORIGINAL)
-		{
-			h_in = fileHeight;
-		}
-		imageDestination.w = w_in;
-		imageDestination.h = h_in;
-	}
-
-	// this method helps set the source of the SDL_texture by creating a rectangle
-	/*void setTextureClip(int x_in, int y_in, int w_in, int h_in)
-	{
-		// if all the values are negative that means set the clip to a nullptr
-		// this will set the source raectangle to the entire image in the SDL_RenderCopy method
-		if (w_in < 0 && h_in < 0 && x_in < 0 && y_in < 0)
-		{
-			isEntireSource = true;
-			return;
-		}
-
-		if (w_in == WIDTH_ORIGINAL)
-		{
-			w_in = fileWidth;
-		}
-
-		if (h_in == HEIGHT_ORIGINAL)
-		{
-			h_in = fileHeight;
-		}
-
-		// center in the middle width of the window
-		if (x_in == CENTER_X)
-		{
-			x_in = windowWidth/2 - fileWidth / 2;
-		}
-
-		// center in middle height of the window
-		if (y_in == CENTER_Y)
-		{
-			y_in = windowHeight/2 - fileHeight / 2;
-		}
-
-		textureClip = { x_in, y_in, w_in, h_in };
-	}*/
-
-	// this method helps set the destination of the SDL_texture by creating a rectangle
-	/*void setImageDestination(int x_in, int y_in, int w_in, int h_in)
-	{
-
-		// if they are all negative, meaning no parameter was passed in to the original function then it means take up the entire screen
-		// by making the imageDestination nullptr
-		if (w_in < 0 && h_in < 0 && x_in < 0 && y_in < 0)
-		{
-			isEntireDestination = true;
-			return;
-		}
-
-		if (w_in == WIDTH_ORIGINAL)
-		{
-			w_in = fileWidth;
-		}
-		if (h_in == HEIGHT_ORIGINAL)
-		{
-			h_in = fileHeight;
-		}
-
-		// center in the middle width of the window
-		if (x_in == CENTER_X)
-		{
-			x_in = windowWidth / 2 -w_in / 2;
-		}
-
-		if (x_in == LEFT)
-		{
-			x_in = 50;
-		}
-
-		if (x_in == RIGHT)
-		{
-			x_in = windowWidth - 50 - w_in;
-		}
-
-		// center in middle height of the window
-		if (y_in == CENTER_Y)
-		{
-			y_in = windowHeight / 2 - h_in / 2;
-		}
-
-		if (y_in == TOP)
-		{
-			y_in = 20;
-		}
-
-		if (y_in == BOTTOM)
-		{
-			y_in = windowHeight - 50;
-		}
-
-		imageDestination = { x_in, y_in, w_in, h_in };
-	}*/
-
-	// this method is used to create image textures that can be rendered
-	bool loadFromFile(std::string path);
-
-	// this method is used to create text textures that can be rendered
-	bool loadFromRenderedText(std::string filePath, int size, std::string textureText, SDL_Color textColor);
-
-	// this method is used to edit a texture to make it look selected
-	void setSelected(bool selected_in);
-
-	// this is used to deallocate an ArcadeTexture object by freeing the SDL texture
-	void myFree();
-
-	// this method renders an ArcadeTexture to the screen using the underlying SDL texture
-	void render(SDL_Renderer* renderer);
-
-	bool isEntireSource;
-	bool isEntireDestination;
+		std::string gameName;
+		std::string name;
+		int score;
+	};
 private:
-	//The actual hardware texture
-	SDL_Texture* texture ;
+	std::vector<Entity*> entities;
+	std::vector<Entity*> movingEntities;
 
-	//Renderer
-	SDL_Renderer* texRen;
+	void* parentNode;
 
-	// the font object
-	TTF_Font* fontRen;
+	void* pauseScreen;
+	void* gameOverScreen;
 
-	// image rectangle destination
-	SDL_Rect imageDestination;
-
-	// rectangle crop
-	SDL_Rect textureClip;
-
-	// original image dimensions
-	int fileWidth;
-	int fileHeight;
+	bool gameState;
+	bool isNewGame;
 };
-
 #endif
 
